cb3bad0f62709352e22bcba51a3ac77e5bccfc2d
Lars Buis <buislars05@gmail.com>
2024-10-09 20:26:47 +0200

    nog nie af

---
+++ VictuZWebProject/Migrations/VictuZ_Lars_DbMigrations/20241009121149_test6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VictuZWebProject.Migrations.VictuZ_Lars_DbMigrations
+{
+    /// <inheritdoc />
+    public partial class test6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Activity",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Activity");
+        }
+    }
+}
