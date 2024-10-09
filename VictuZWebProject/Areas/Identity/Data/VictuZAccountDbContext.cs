90bba8ea2ae0483ec70d0bc015b76202d843bbce
dnsvh <2205686herten@zuyd.nl>
2024-10-09 11:58:50 +0200

    Add project files.

---
+++ VictuZWebProject/Areas/Identity/Data/VictuZAccountDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VictuZWebProject.Areas.Identity.Data;
+
+namespace VictuZWebProject.Data;
+
+public class VictuZAccountDbContext : IdentityDbContext<AppUser>
+{
+    public VictuZAccountDbContext(DbContextOptions<VictuZAccountDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ VictuZWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VictuZWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
