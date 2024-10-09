26861262b25b488ac42cea7d5329ea7dee0742d9
dnsvh <2205686herten@zuyd.nl>
2024-10-09 13:33:50 +0200

    account ui update, customized the login register and account management pages, still incomplete

--- VictuZWebProject/Areas/Identity/Data/AppUser.cs
+++ VictuZWebProject/Areas/Identity/Data/AppUser.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace VictuZWebProject.Areas.Identity.Data;
 
-// Add profile data for application users by adding properties to the AppUser class
 public class AppUser : IdentityUser
 {
     public string FirstName { get; set; }
