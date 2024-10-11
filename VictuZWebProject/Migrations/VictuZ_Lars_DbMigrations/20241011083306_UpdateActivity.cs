d1da2f5c27dafdf98a8db2e7093ad73c7a3b35e0
KingCaellum <2104153fijneman@zuyd.nl>
2024-10-11 11:15:10 +0200

    Database fix

---
+++ VictuZWebProject/Migrations/VictuZ_Lars_DbMigrations/20241011083306_UpdateActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VictuZWebProject.Migrations.VictuZ_Lars_DbMigrations
+{
+    /// <inheritdoc />
+    public partial class UpdateActivity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MemberVisibilityEnd",
+                table: "Activity",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MemberVisibilityStart",
+                table: "Activity",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MembersOnlyRegistration",
+                table: "Activity",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OnlyVisibleMembers",
+                table: "Activity",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MemberVisibilityEnd",
+                table: "Activity");
+
+            migrationBuilder.DropColumn(
+                name: "MemberVisibilityStart",
+                table: "Activity");
+
+            migrationBuilder.DropColumn(
+                name: "MembersOnlyRegistration",
+                table: "Activity");
+
+            migrationBuilder.DropColumn(
+                name: "OnlyVisibleMembers",
+                table: "Activity");
+        }
+    }
+}
