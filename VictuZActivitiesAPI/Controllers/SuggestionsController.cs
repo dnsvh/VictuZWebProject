8c7534b5c132d7327191c8905fef5308a09f9810
Denise <2205686herten@zuyd.nl>
2024-10-13 19:47:20 +0200

    removed old api, made new one thats actually dependent

---
+++ VictuZActivitiesAPI/Controllers/SuggestionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VictuZWebProject.Models;
+using VictuZ_Lars.Data;
+
+namespace VictuZActivitiesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuggestionsController : ControllerBase
+    {
+        private readonly VictuZ_Lars_Db _context;
+
+        public SuggestionsController(VictuZ_Lars_Db context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Suggestions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Suggestion>>> GetSuggestion()
+        {
+            return await _context.Suggestion.ToListAsync();
+        }
+
+        // GET: api/Suggestions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Suggestion>> GetSuggestion(int id)
+        {
+            var suggestion = await _context.Suggestion.FindAsync(id);
+
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            return suggestion;
+        }
+
+        // PUT: api/Suggestions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSuggestion(int id, Suggestion suggestion)
+        {
+            if (id != suggestion.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(suggestion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SuggestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Suggestions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Suggestion>> PostSuggestion(Suggestion suggestion)
+        {
+            _context.Suggestion.Add(suggestion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSuggestion", new { id = suggestion.Id }, suggestion);
+        }
+
+        // DELETE: api/Suggestions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSuggestion(int id)
+        {
+            var suggestion = await _context.Suggestion.FindAsync(id);
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Suggestion.Remove(suggestion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SuggestionExists(int id)
+        {
+            return _context.Suggestion.Any(e => e.Id == id);
+        }
+    }
+}
