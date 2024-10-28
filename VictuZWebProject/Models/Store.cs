f8e1d1a919b8cfb368760132f99b2cc62cc42e1a
C0rpsy <2065665hoof@zuyd.nl>
2024-10-28 13:48:34 +0100

    Update Store model: Price to decimal, add Size property
    Changed Price property in Store model from float to decimal for better precision in monetary values. Added new Size property to Store model. Updated corresponding views (Create, Delete, Details, Edit) to handle the new Size property. Added migration class StoreUpdate4 to manage schema changes. Auto-generated StoreUpdate4.Designer.cs to reflect model updates.

--- VictuZWebProject/Models/Store.cs
+++ VictuZWebProject/Models/Store.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string ? Size { get; set; }
-        public float Price { get; set; }
+        public decimal Price { get; set; }
         public string ? ImageUrl { get; set; }
         public string Category { get; set; }
         public int Stock { get; set; }
