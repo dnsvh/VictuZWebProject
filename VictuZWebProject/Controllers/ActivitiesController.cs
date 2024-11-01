03b937d00953af718ae7f178fb8f7250fd84c1d8
dnsvh <2205686herten@zuyd.nl>
2024-11-01 11:13:26 +0100

    Fix because i accidentally messed up a pull req

--- VictuZWebProject/Controllers/ActivitiesController.cs
+++ VictuZWebProject/Controllers/ActivitiesController.cs
@@ -123,7 +123,7 @@ namespace VictuZ_Lars.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
-        public async Task<IActionResult> Create([Bind("ActivityId,Name,Body,Location,ImageUrl,Registered,MaxCapacity,DatePublished,DateDue")] Activity activity)
+        public async Task<IActionResult> Create([Bind("ActivityId,Name,Body,Host,Location,ImageUrl,Registered,MaxCapacity,DatePublished,DateDue")] Activity activity)
         {
             if (ModelState.IsValid)
             {
@@ -157,7 +157,7 @@ namespace VictuZ_Lars.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ActivityId,Name,Body,Location,ImageUrl,Registered,MaxCapacity,DatePublished,DateDue")] Activity activity)
+        public async Task<IActionResult> Edit(int id, [Bind("ActivityId,Name,Body,Host,Location,ImageUrl,Registered,MaxCapacity,DatePublished,DateDue")] Activity activity)
         {
             if (id != activity.ActivityId)
             {
