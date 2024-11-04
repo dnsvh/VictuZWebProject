1f2a241a5e23f32890af876fbac9af5fb5b32b45
Lars Buis <buislars05@gmail.com>
2024-11-04 13:05:27 +0100

    admin-dash

---
+++ VictuZWebProject/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VictuZWebProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
